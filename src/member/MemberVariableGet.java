67a347995a13056713ed4a443f6b37465e374712
yasuda <samescar@mse.biglobe.ne.jp>
2017-09-04 14:43:01 +0900

    no message

--- src/member/MemberVariableGet.java
+++ src/member/MemberVariableGet.java
@@ -2,43 +2,59 @@ package member;
 
 import java.lang.reflect.Field;
 
+/**
+ * 指定クラスのメンバを割り出す
+ *
+ * @期待値
+ *
+ * name = nameA
+ * id = 12345
+ * privateName = [private]
+ * privateId = [private]
+ *
+ * name
+ * id
+ * privateName
+ * privateId
+ *
+ */
+
 public class MemberVariableGet {
 
-    /**
-     * メンバ変数を全て取得する
-     */
-    public static void main(String arg[]) {
+	/**
+	 * メンバ変数を全て取得する
+	 */
+	public static void main(String arg[]) {
 
-    	// インスタンスから取得
-    	Example ex = new Example();
-        for (Field field : ex.getClass().getDeclaredFields()) {
-            try {
+		// インスタンスから取得
+		Example ex = new Example();
+		for (Field field : ex.getClass().getDeclaredFields()) {
+			try {
 				System.out.printf(field.getName() + " = " + field.get(ex) + "\n");
 			} catch (IllegalArgumentException | IllegalAccessException e) {
-				//e.printStackTrace();
+				// e.printStackTrace();
 				System.out.printf(field.getName() + " = " + "[private]" + "\n");
 			}
-        }
+		}
 
-        System.out.println();
+		System.out.println();
 
-        // スタテックで取得(インスタンスメンバの値は取れない？)
-        for (Field field : Example.class.getDeclaredFields()) {
-				System.out.printf(field.getName() + "\n");
-        }
+		// スタテックで取得(インスタンスメンバの値は取れない？)
+		for (Field field : Example.class.getDeclaredFields()) {
+			System.out.printf(field.getName() + "\n");
+		}
 
-    }
-
+	}
 
 }
 
 class Example {
-    public String name = "nameA";
-    public Integer id = 12345;
+	public String name = "nameA";
+	public Integer id = 12345;
 
-    @SuppressWarnings("unused")
-    private String privateName = "xxx";
-    @SuppressWarnings("unused")
+	@SuppressWarnings("unused")
+	private String privateName = "xxx";
+	@SuppressWarnings("unused")
 	private static Integer privateId = 999;
 
 }
