102b2fba07baf1a246ba0d3d13ecfc551af2ca01
yasuda <samescar@mse.biglobe.ne.jp>
2017-09-04 19:20:16 +0900

    no message

--- src/thread_THreadLocal/MyApp.java
+++ src/thread_THreadLocal/MyApp.java
@@ -1,27 +1,42 @@
 package thread_THreadLocal;
 
+//Thread
+
+//class MyRunnable implements Runnable { // 関数型インターフェース
+//@Override
+//public void run() {
+//  for (int i = 0; i < 500; i++) {
+//    System.out.print('*');
+//  }
+//}
+//}
+
 public class MyApp {
 
 	public static void main(String[] args) {
+		// MyRunnable r = new MyRunnable();
+		// Thread t = new Thread(r);
+		// t.start();
 
-		MyRunnable runnable = new MyRunnable();
-		Thread thread = new Thread(runnable);
-		thread.start();
+		// ラムダ式
+		// (引数) -> {処理}
+		// new Thread(new Runnable() {
+		// @Override
+		// public void run() {
+		// for (int i = 0; i < 500; i++) {
+		// System.out.print('*');
+		// }
+		// }
+		// }).start();
+		new Thread(() -> {
+			for (int i = 0; i < 500; i++) {
+				System.out.print('*');
+			}
+		}).start();
 
 		for (int i = 0; i < 500; i++) {
-			System.out.print('+');
-		}
-
-	}
-
-}
-
-class MyRunnable implements Runnable {
-
-	@Override
-	public void run() {
-		for (int i = 0; i < 500; i++) {
-			System.out.print('*');
+			System.out.print('.');
 		}
 	}
+
 }
